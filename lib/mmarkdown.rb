f330ee6e0f22129cfdf026d0ea0a9136ee3c257e
Hiroyuki Tanaka <hryktnk@gmail.com>
2014-05-06 21:18:43 +0200

    MMarkdown#toc_html

--- lib/mmarkdown.rb
+++ lib/mmarkdown.rb
@@ -4,26 +4,24 @@ require "redcarpet"
 require "#{__dir__}/mmarkdown/version"
 
 class MMarkdown
-
   def initialize md_string
-    md_mathml = equations_to_mathml(md_string)
-
-    renderer = Redcarpet::Render::HTML
+    @md_mathml = equations_to_mathml(md_string)
 
     parse_extensions = {no_intra_emphasis: true,
                         tables: true,
                         fenced_code_blocks: true,
                         autolink: true,
-                        disable_indented_code_blocks: true,
+                        disable_indented_code_blocks: false,
+                        superscript: true,
                         underline: true,
                         highlight: true,
                         footnotes: true}
 
     render_extensions = {with_toc_data: true}
 
-    render = renderer.new(render_extensions)
+    render = Redcarpet::Render::HTML.new(render_extensions)
 
-    @md = Redcarpet::Markdown.new(render, parse_extensions).render(md_mathml)
+    @md = Redcarpet::Markdown.new(render, parse_extensions).render(@md_mathml)
   end
 
   def equations_to_mathml string
@@ -39,6 +37,13 @@ class MMarkdown
     }
 
     return string_mathml
+  end
+
+  def toc_html nesting_level = 3
+    render_toc = Redcarpet::Render::HTML_TOC.new
+    toc_html = Redcarpet::Markdown.new(render_toc).render(@md_mathml)
+
+    return toc_html
   end
 
   def to_str
--- test/test_mmarkdown.rb
+++ test/test_mmarkdown.rb
@@ -13,5 +13,48 @@ class MMarkdownTest < MiniTest::Test
     assert_equal(Nokogiri::HTML::DocumentFragment.parse(html_expected).to_html,
                  Nokogiri::HTML::DocumentFragment.parse(html_actual).to_html)
   end
+
+  def test_toc_html
+    md_string = File.open("#{__dir__}/test.md").read
+
+    mmarkdown = MMarkdown.new(md_string)
+    toc_actual = mmarkdown.toc_html
+
+    toc_expected = <<EOF
+<ul>
+<li>
+<a href="#test">Test</a>
+<ul>
+<li>
+<a href="#equation">Equation</a>
+</li>
+<li>
+<a href="#markdown">Markdown</a>
+<ul>
+<li>
+<a href="#list">List</a>
+</li>
+<li>
+<a href="#quote">Quote</a>
+</li>
+<li>
+<a href="#table">Table</a>
+</li>
+<li>
+<a href="#syntax-highlight">Syntax highlight</a>
+</li>
+<li>
+<a href="#link">Link</a>
+</li>
+</ul>
+</li>
+</ul>
+</li>
+</ul>
+EOF
+
+    assert_equal(toc_expected, toc_actual)
+
+  end
 end
 
